fb1e62c6fd6f5dbbf08d9a5a175c2371c98e8f59
Aimar <aimar@snapit.ee>
2018-05-18 17:01:35 +0200

    rvsp route added

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import Landing from '../containers/Landing';
 import Invite from '../containers/Invite';
+import Rvsp from '../containers/Rvsp';
 
 const App = () => (
   <div>
     <Switch>
       <Route path="/" exact component={Landing} />
       <Route path="/invite" exact component={Invite} />
+      <Route path="/Rvsp" exact component={Rvsp} />
     </Switch>
   </div>
 );
